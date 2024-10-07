de089cf83a18c8c60766880326fcd4cd4c4cabf3
cativeiro-code <lucas231002@outlook.com>
2024-10-07 11:37:58 -0300

    adding main movement

--- Source/CPPTuto/Player/playercharacter.cpp
+++ Source/CPPTuto/Player/playercharacter.cpp
@@ -2,13 +2,56 @@
 
 
 #include "CPPTuto/Player/playercharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Controller.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 Aplayercharacter::Aplayercharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("camera"));
+	camera->SetupAttachment(RootComponent);
+	camera->bUsePawnControlRotation=true;
 
+}
+
+void Aplayercharacter::move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector 
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement 
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void Aplayercharacter::look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
 
 // Called when the game starts or when spawned
@@ -28,7 +71,24 @@ void Aplayercharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void Aplayercharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &Aplayercharacter::move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &Aplayercharacter::look);
+	}
+	
 
 }
 
--- Source/CPPTuto/Player/playercharacter.h
+++ Source/CPPTuto/Player/playercharacter.h
@@ -6,14 +6,35 @@
 #include "GameFramework/Character.h"
 #include "playercharacter.generated.h"
 
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
 UCLASS()
 class CPPTUTO_API Aplayercharacter : public ACharacter
 {
 	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
 public:
 	// Sets default values for this character's properties
 	Aplayercharacter();
+protected:
+	void move(const	FInputActionValue& Value);
+	void look(const	FInputActionValue& Value);
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,5 +46,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* camera;
 
 };
